d1460272bf344298289be07cdde846487ea1f3b3
unknown <cedced19@gmail.com>
2014-09-01 20:22:14 +0200

    Fix Uncss bug

--- dev/scripts/github.js
+++ dev/scripts/github.js
@@ -1,27 +1,27 @@
 (function () {
 
       function addRepo(repo) {
-        var $item = $("<li>").addClass("repo");
+        var $item = $('<li>').addClass('repo');
         var name = repo.name
-        $("<a>").text(name.charAt(0).toUpperCase() + name.substring(1).toLowerCase()).attr("href", repo.html_url).addClass("name-repo").appendTo($item);
-        $("<p>").text(repo.description).addClass("desc-repo").appendTo($item);
-        $("<span>").addClass("language").text(repo.language).appendTo($item);
+        $('<a>').text(name.charAt(0).toUpperCase() + name.substring(1).toLowerCase()).attr('href', repo.html_url).addClass('name-repo').appendTo($item);
+        $('<p>').text(repo.description).addClass('desc-repo').appendTo($item);
+        $('<span>').addClass('gray').text(repo.language).appendTo($item);
 
         if (repo.fork) {
-          $("<br>").appendTo($item)
-          $("<span>").addClass("fork").text("Forked").appendTo($item);
+          $('<br>').appendTo($item)
+          $('<span>').addClass('gray').text('Forked').appendTo($item);
         }
 
-        $item.appendTo("#repos");
+        $item.appendTo('#repos');
       }
 
       function addRepos(repos, page) {
         repos = repos || [];
         page = page || 1;
 
-        var uri = "https://api.github.com/users/cedced19/repos?callback=?"
-                + "&per_page=100"
-                + "&page="+page;
+        var uri = 'https://api.github.com/users/cedced19/repos?callback=?'
+                + '&per_page=100'
+                + '&page='+page;
 
         $.getJSON(uri, function (result) {
           if (result.data && result.data.length > 0) {
