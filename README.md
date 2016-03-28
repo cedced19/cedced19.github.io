ba468a1d54e5b8f4391abebce4da112b3216df56
Cédric <cedced19@gmail.com>
2016-03-28 10:50:02 +0200

    Add Gulp serve system and change home page

--- README.md
+++ README.md
@@ -3,9 +3,5 @@
 [![devDependency Status](https://david-dm.org/cedced19/cedced19.github.io/dev-status.svg)](https://david-dm.org/cedced19/cedced19.github.io#info=devDependencies)
 
 Hi! Here is the source code of my website!
-
-All CSS uncompressed are [here](//github.com/cedced19/cedced19.github.io/tree/master/dev/styles).
-
-All JavaScript uncompressed are [here](//github.com/cedced19/cedced19.github.io/tree/master/dev/scripts).
-
-Do `npm run build` to compile.
+Do `npm run build` to compile, and `npm run serve` to test.
+Actually all files at the root of the website are compiled but you can found sources in _dev_ folder.
--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     uglify = require('gulp-uglify'),
     minifyCss = require('gulp-minify-css'),
+    connect = require('gulp-connect'),
+    opn = require('opn'),
     htmlmin = require('gulp-htmlmin');
 
 
@@ -14,8 +16,8 @@ gulp.task('html', function () {
     return gulp.src('dev/**/*.html')
         .pipe(assets)
         .pipe(gulpif('*.js', uglify()))
-        .pipe(gulpif('*.css', autoprefixer({ 
-            browsers: ['last 2 versions', 'ie 8', 'ie 9'] 
+        .pipe(gulpif('*.css', autoprefixer({
+            browsers: ['last 2 versions', 'ie 8', 'ie 9']
         })))
         .pipe(gulpif('*.css', minifyCss()))
         .pipe(assets.restore())
@@ -24,4 +26,24 @@ gulp.task('html', function () {
         .pipe(gulp.dest('./'));
 });
 
+
+gulp.task('reload', function () {
+  return gulp.src('dev/**/**.*')
+    .pipe(connect.reload());
+});
+
+gulp.task('connect', function (done) {
+  connect.server({
+    root: './',
+    port: 8080,
+    livereload: true
+  });
+  opn('http://localhost:8080/dev/', done);
+});
+
+gulp.task('watch', function () {
+  gulp.watch('dev/**/**.*', ['reload']);
+});
+
+gulp.task('serve', ['connect', 'watch']);
 gulp.task('default', ['html']);
